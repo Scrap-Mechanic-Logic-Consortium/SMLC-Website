18bd3b3787d028789be711d7049af6dd2bc6df73
niknal357 <46793548+niknal357@users.noreply.github.com>
2024-03-16 00:41:39 +0100

    cool new features to auto-hyperlink definitions

--- repoPull.js
+++ repoPull.js
@@ -29,12 +29,101 @@ async function cloneRepo(repoUrl) {
     }
 }
 
+function detectDefinedTerms(path = repoTempFolder + 'SMLC-Terminology/terminology/', folder = '/terminology/') {
+    const files = fs.readdirSync(path);
+    terms = {};
+    for (const file of files) {
+        if (!file.endsWith('.md') && !file.endsWith('.markdown') && !file.endsWith('.mdx')) {
+            // check if file is a folder
+            if (fs.lstatSync(path +
+                file).isDirectory()) {
+                const childTerms = detectDefinedTerms(path + file + '/', folder + file + '/');
+                for (const term in childTerms) {
+                    terms[term] = childTerms[term];
+                }
+            }
+            continue;
+        }
+        const content = fs.readFileSync(path + file, 'utf-8');
+        const lines = content.split('\n');
+        let lineTerms = [];
+        for (const line of lines) {
+            if (!line.includes("**")) {
+                continue;
+            }
+            let segments = line.split('**');
+            for (let i = 1; i < segments.length; i += 2) {
+                lineTerms.push(segments[i]);
+                // remove the file extension from the link name
+                const linkName = file.split('.')[0];
+                terms[segments[i].toLocaleLowerCase()] = {
+                    file: folder + linkName,
+                    excerpt: null,
+                }
+            }
+        }
+        // split the file into sentences so we can find the terms in context (and show them on hover)
+        // split by ., !, and ? and \n / \r
+        let sentences = content.split(/[\.\!\?\n\r]/);
+        for (const sentence of sentences) {
+            for (const term of lineTerms) {
+                if (sentence.includes("**" + term + "**")) {
+                    if (!terms[term.toLocaleLowerCase()]) {
+                        return;
+                    }
+                    let trimmed = sentence.trim();
+                    // trim off spaces and dashes
+                    while (trimmed.startsWith(' ') || trimmed.startsWith('-')) {
+                        trimmed = trimmed.slice(1);
+                    }
+                    terms[term.toLocaleLowerCase()].excerpt = trimmed;
+                }
+            }
+        }
+    }
+    return terms;
+}
+
+function insertTermHyperlinks(terms, path = repoTempFolder + 'SMLC-Standards/standards/', folder = '/standards/') {
+    const files = fs.readdirSync(path);
+    console.log(files);
+    for (const file of files) {
+        if (!file.endsWith('.md') && !file.endsWith('.markdown') && !file.endsWith('.mdx')) {
+            // check if file is a folder
+            if (fs.lstatSync(path +
+                file).isDirectory()) {
+                insertTermHyperlinks(terms, path + file + '/', folder + file + '/');
+            }
+            continue;
+        }
+        const content = fs.readFileSync(path + file, 'utf-8');
+        let newContent = content;
+        // find FIRST instance of term in file (if it exists) and replace it with a hyperlink
+        for (const term in terms) {
+            if (newContent.includes(term)) {
+                let termLink = `[${term}](` + terms[term].file + ")";
+                // replace first instance of term with hyperlink
+                splitContent = newContent.split(term);
+                newContent = splitContent[0] + termLink + splitContent.slice(1).join(term);
+            }
+        }
+        if (newContent !== content) {
+            fs.writeFileSync(path + file, newContent);
+            console.log('Inserted term hyperlinks into ' + file);
+        }
+    }
+}
 async function main() {
     await deleteAndRecreateRepoTemp();
 
     for (const repoUrl of gitRepos) {
         await cloneRepo(repoUrl);
     }
+    let terms = detectDefinedTerms();
+    // sort terms by descending length (by key) so we can replace longer terms first
+    terms = Object.fromEntries(Object.entries(terms).sort((a, b) => b[0].length - a[0].length));
+    console.log(terms);
+    insertTermHyperlinks(terms);
 }
 
 main();
